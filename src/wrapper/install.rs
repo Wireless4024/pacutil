fd99f2db477b9970fb9c51b3fd0fd0f436785c34
wireless4024 <wireless4024@proton.me>
2022-09-06 01:42:36 +0700

    chore: Install multiple package

--- src/wrapper/install.rs
+++ src/wrapper/install.rs
@@ -1,8 +1,7 @@
-use std::process::{Command, Stdio};
-
 use anyhow::Result;
 use serde::Deserialize;
 use tracing::info;
+
 use crate::wrapper::pacman;
 
 #[derive(Deserialize, Debug)]
@@ -26,6 +25,42 @@ pub fn install_pkg(pkg: &InstallablePackage) -> Result<()> {
 	}
 	info!("Installing {}/{}",pkg.repo,pkg.name);
 	let mut child = pacman(&cmd)?;
+	child.wait()?;
+	Ok(())
+}
+
+pub fn install_pkgs(pkgs: &[impl AsRef<InstallablePackage>]) -> Result<()> {
+	let mut deps = pkgs.iter()
+		.map(|it| it.as_ref())
+		.filter(|it| it.as_dependency)
+		.map(|p| format!("{}/{}", p.repo, p.name))
+		.collect::<Vec<_>>();
+	let mut explicit = pkgs.iter()
+		.map(|it| it.as_ref())
+		.filter(|it| !it.as_dependency)
+		.map(|p| format!("{}/{}", p.repo, p.name))
+		.collect::<Vec<_>>();
+
+	let (deps_len, exp_len) = (deps.len(), explicit.len());
+
+	// TODO: redirect stdout to file
+	let mut exps_cmd = Vec::new();
+	exps_cmd.push(String::from("-S"));
+	exps_cmd.push(String::from("--noconfirm"));
+	exps_cmd.extend(explicit);
+
+	info!("Installing {} packages",exp_len);
+	let mut child = pacman(&exps_cmd)?;
+	child.wait()?;
+
+	let mut deps_cmd = Vec::new();
+	deps_cmd.push(String::from("-S"));
+	deps_cmd.push(String::from("--noconfirm"));
+	deps_cmd.extend(deps);
+	deps_cmd.push(String::from("--asdeps"));
+
+	info!("Installing {} packages as dependency",deps_len);
+	let mut child = pacman(&deps_cmd)?;
 	child.wait()?;
 	Ok(())
 }
